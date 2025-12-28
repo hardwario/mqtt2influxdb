b5f2c03e6e6f4359918e65a2299443351eae3003
Pavel Hübner <pavel.hubner@hardwario.com>
2025-12-28 18:29:38 +0100

    chore: Update package init with v2.0.0 and exports

--- mqtt2influxdb/__init__.py
+++ mqtt2influxdb/__init__.py
@@ -1,3 +1,15 @@
-#!/usr/bin/env python3
+"""mqtt2influxdb - MQTT to InfluxDB v3 bridge for IoT applications."""
 
-__version__ = '0.0.0'
+__version__ = "2.0.0"
+__author__ = "HARDWARIO a.s."
+
+from .config import Config, ConfigError, load_config
+from .mqtt2influxdb import Mqtt2InfluxDB
+
+__all__ = [
+    "__version__",
+    "Config",
+    "ConfigError",
+    "load_config",
+    "Mqtt2InfluxDB",
+]
