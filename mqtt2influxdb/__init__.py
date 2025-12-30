5d2ca8f4e209796ba59a7629e44e68d095e40758
Pavel Hübner <pavel.hubner@hardwario.com>
2025-12-30 14:04:17 +0100

    ci: Add uv.lock version check, bump to v2.0.6

--- mqtt2influxdb/__init__.py
+++ mqtt2influxdb/__init__.py
@@ -1,6 +1,6 @@
 """mqtt2influxdb - MQTT to InfluxDB v3 bridge with flexible JSONPath transformation."""
 
-__version__ = "2.0.5"
+__version__ = "2.0.6"
 __author__ = "HARDWARIO a.s."
 
 from .config import Config, ConfigError, load_config
